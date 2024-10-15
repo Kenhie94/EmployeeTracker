c236ef974d4bec44b1c5bb02a79faf84e951278f
Kenjy Jap <kenhiebusiness@gmail.com>
2024-10-14 17:37:33 -0700

    Implemented all the functions

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ function startInquirer() {
         type: "list",
         name: "action",
         message: "What would you like to do?",
-        choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Role", "Add Role", "View All Department", "Add Department", "Exit"],
+        choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Exit"],
       },
     ])
     .then(async (answers) => {
@@ -38,21 +38,21 @@ function startInquirer() {
         case "Add Employee":
           await addEmployee();
           break;
-        // case "Update Employee Role":
-        //   await updateEmployeeRole();
-        //   break;
-        // case "View All Roles":
-        //   await viewAllRoles();
-        //   break;
-        // case "Add Role":
-        //   await addRole();
-        //   break;
-        // case "View All Departments":
-        //   await viewAllDepartments();
-        //   break;
-        // case "Add Department":
-        //   await addDepartment();
-        //   break;
+        case "Update Employee Role":
+          await updateEmployeeRole();
+          break;
+        case "View All Roles":
+          await viewAllRoles();
+          break;
+        case "Add Role":
+          await addRole();
+          break;
+        case "View All Departments":
+          await viewAllDepartments();
+          break;
+        case "Add Department":
+          await addDepartment();
+          break;
         case "Exit":
           console.log("Goodbye!");
           process.exit(0);
@@ -137,6 +137,129 @@ async function addEmployee(): Promise<void> {
     startInquirer();
   } catch (err) {
     console.error("Error adding employee", err);
+  }
+}
+
+async function updateEmployeeRole(): Promise<void> {
+  try {
+    const employeeQuery: QueryResult = await pool.query("SELECT id, first_name, last_name FROM employee");
+    const roleQuery: QueryResult = await pool.query("SELECT id, title FROM role");
+
+    const employeeChoices = employeeQuery.rows.map((employee) => ({
+      name: `${employee.first_name} ${employee.last_name}`,
+      value: employee.id,
+    }));
+    const roleChoices = roleQuery.rows.map((role) => ({
+      name: role.title,
+      value: role.id,
+    }));
+
+    const answers = await inquirer.prompt([
+      {
+        type: "list",
+        name: "chosenEmployee",
+        message: "Which employee's role do you want to update?",
+        choices: employeeChoices,
+      },
+      {
+        type: "list",
+        name: "chosenRole",
+        message: "Which role do you want to assign the selected employee",
+        choices: roleChoices,
+      },
+    ]);
+
+    const { chosenEmployee, chosenRole } = answers;
+
+    await pool.query(`UPDATE employee SET role_id = $1 WHERE id = $2`, [chosenRole, chosenEmployee]);
+    console.log(`Employee's role has been updated successfully`);
+    startInquirer();
+  } catch (err) {
+    console.error("Error updating employee's role:", err);
+  }
+}
+
+async function viewAllRoles(): Promise<void> {
+  try {
+    const result: QueryResult = await pool.query("SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id");
+
+    console.table(result.rows);
+    startInquirer();
+  } catch (err) {
+    console.error("Error fetching roles:", err);
+  }
+}
+
+async function addRole(): Promise<void> {
+  try {
+    const departmentQuery: QueryResult = await pool.query("SELECT id, name FROM department");
+
+    const departmentChoices = departmentQuery.rows.map((department) => ({
+      name: department.name,
+      value: department.id,
+    }));
+
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "newRole",
+        message: "What is the title of the new role?",
+      },
+      {
+        type: "input",
+        name: "newRoleSalary",
+        message: "What is the salary of this new role?",
+        validate: (input) => {
+          return !isNaN(parseFloat(input)) || "Please enter a valid number";
+        },
+      },
+      {
+        type: "list",
+        name: "chosenDepartment",
+        message: "Which department is this role in?",
+        choices: departmentChoices,
+      },
+    ]);
+
+    const { newRole, newRoleSalary, chosenDepartment } = answers;
+
+    await pool.query(`INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)`, [newRole, parseFloat(newRoleSalary), chosenDepartment]);
+
+    console.log(`${newRole} added succesfully`);
+    startInquirer();
+  } catch (err) {
+    console.error("Error adding role", err);
+  }
+}
+
+async function viewAllDepartments(): Promise<void> {
+  try {
+    const result: QueryResult = await pool.query(`SELECT * FROM department`);
+    console.table(result.rows);
+    startInquirer();
+  } catch (err) {
+    console.error("Error fetching department:", err);
+  }
+}
+
+async function addDepartment(): Promise<void> {
+  try {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the new department?",
+      },
+    ]);
+
+    const { newDepartment } = answers;
+
+    await pool.query("INSERT INTO department (name) VALUES ($1)", [newDepartment]);
+
+    console.log(`${newDepartment} added successfully`);
+    startInquirer();
+  } catch (err) {
+    console.error("Error adding department,", err);
   }
 }
 
